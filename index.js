a6dd8aa39532c79d10754e5c62b377e082c99eec
Eunoia-12 <adrianadsouza0405@gmail.com>
2024-10-13 08:57:38 -0400

    Minor changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 let myLeads = []
-let oldleads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
